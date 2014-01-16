e16bf58c33e5ae2bc7a5bc30799b036f9499190c
Max Rottenkolber <max@mr.gy>
2014-01-16 02:09:51 +0100

    Added TRIVIAL-FEED.XML:PARSE-XSD-TIME.

--- xml.lisp
+++ xml.lisp
@@ -13,3 +13,19 @@
                           (string child)
                           (cons (toxml child)))
                         out))))
+(defun parse-xsd-time (date-string)
+  (handler-case ; Don't throw errors.
+      (if (string-equal "-" date-string :end2 1)
+          (parse-xsd-time (subseq date-string 1))
+          (parse-time
+           (format nil "~a ~a~@[ ~a~]"
+                   (subseq date-string 0 10)
+                   (subseq date-string 11 16)
+                   (when (> (length date-string) 19)
+                     (let ((zone (subseq date-string 19)))
+                       (unless (string-equal "Z" zone :end2 1)
+                         (format nil "~a~a"
+                                 (subseq zone 0 3)
+                                 (subseq zone 4 6))))))))
+    (error () nil))) ; Return NIL on failure.
+
