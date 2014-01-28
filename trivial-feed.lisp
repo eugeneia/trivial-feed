8b54bb4b791fb64968174cdfee9a80c67b26f99b
Max Rottenkolber <max@mr.gy>
2014-01-28 21:25:42 +0100

    Renamed parameter to PARSE-FEED from STREAM to S to match XMLS:PARSE .

--- trivial-feed.lisp
+++ trivial-feed.lisp
@@ -3,9 +3,9 @@
 
 (in-package :trivial-feed)
 
-(defun parse-feed (stream &optional (fallback-date (get-universal-time)))
+(defun parse-feed (s &optional (fallback-date (get-universal-time)))
   (let ((*date* fallback-date)
-        (feed-tree (parse stream :compress-whitespace t)))
+        (feed-tree (parse s :compress-whitespace t)))
     (cond ((rss-feed-p feed-tree) (parse-rss feed-tree))
           ((atom-feed-p feed-tree) (parse-atom feed-tree))
           ;; Unsupported format, fail.
