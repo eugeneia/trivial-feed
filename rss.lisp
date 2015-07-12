b49e70ad51d33c1b07e8a8432037e779c744752d
Max Rottenkolber <max@mr.gy>
2015-07-13 00:10:12 +0200

    Add support for "Dublin Core" to TRIVIAL-FEED.RSS::PARSE-RDF-FEED.

--- rss.lisp
+++ rss.lisp
@@ -11,33 +11,55 @@
 
 ;;; RDF parser.
 
+(defun format-rdf-authors (author-nodes)
+  (format nil "~{~a~^, ~}" (mapcar 'node-text author-nodes)))
+
 (defun parse-rdf-node (rdf-nodes)
+  ;; "date", "creator", "contributor" and "language" nodes are defined by
+  ;; Dublin Core (http://web.resource.org/rss/1.0/modules/dc/).
   (let ((link-node
          (find-if (node-by-name "link") rdf-nodes))
+        (date-node
+         (find-if (node-by-name "date") rdf-nodes))
         (title-node
          (find-if (node-by-name "title") rdf-nodes))
+        (author-nodes
+         (append
+          (remove-if-not (node-by-name "creator") rdf-nodes)
+          (remove-if-not (node-by-name "contributor") rdf-nodes)))
         (description-node
-         (find-if (node-by-name "description") rdf-nodes)))
+         (find-if (node-by-name "description") rdf-nodes))
+        (language-node
+         (find-if (node-by-name "language") rdf-nodes)))
     (values
      (and link-node (node-text link-node))
+     (and date-node (parse-date-time (node-text date-node)))
      (and title-node (node-text title-node))
-     (and description-node (node-text description-node)))))
+     (and author-nodes (format-rdf-authors author-nodes))
+     (and description-node (node-text description-node))
+     (and language-node (node-text language-node)))))
 
 (defun parse-rdf-feed (rdf-nodes)
   (let ((channel-node (find-if (node-by-name "channel") rdf-nodes))
         (item-nodes (remove-if-not (node-by-name "item") rdf-nodes)))
-    (multiple-value-bind (link title description)
+    (multiple-value-bind (link date title author description language)
         (parse-rdf-node (node-children channel-node))
-      (make-feed (mapcar (lambda (node)
-                           (multiple-value-bind (link title description)
-                               (parse-rdf-node (node-children node))
-                             (make-feed-item :link link
-                                             :title title
-                                             :description description)))
-                         item-nodes)
-                 :link link
-                 :title title
-                 :description description))))
+      (make-feed
+       (mapcar (lambda (node)
+                 (multiple-value-bind (link date* title author*
+                                       description language*)
+                     (parse-rdf-node (node-children node))
+                   (make-feed-item :link link
+                                   :date (or date* date)
+                                   :title title
+                                   :author (or author* author)
+                                   :description description
+                                   :language (or language* language))))
+               item-nodes)
+       :link link
+       :date date
+       :title title
+       :description description))))
 
 (defun parse-rss-channel (channel-nodes)
   (let ((link-node
