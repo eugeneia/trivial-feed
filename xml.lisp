7206df7bdf2228d971f1dbbaa93abd21f38eb2ee
Max Rottenkolber <max@mr.gy>
2017-02-21 03:30:10 +0100

    …XML:XML-ENCODING: slight refactoring

--- xml.lisp
+++ xml.lisp
@@ -9,6 +9,9 @@
 (defparameter *pi-end*   (string-to-octets "?>"  :external-format :ascii))
 (defparameter *xml*      (string-to-octets "xml" :external-format :ascii))
 
+(defparameter *max-decl-size* 200
+  "XML declarations exceeding *MAX-DECL-SIZE* bytes are ignored.")
+
 (eval-when (:compile-toplevel :load-toplevel)
   (let ((s "[\\s\\r]*")                 ; (#x20 | #x9 | #xD | #xA)+?
         (enc-name "[A-Za-z0-9\\._-]+")) ; [A-Za-z] ([A-Za-z0-9._] | '-')*
@@ -22,29 +25,27 @@
     (external-format-name (make-external-format (or a b)))))
 
 (defun xml-encoding (in)
-  (let ((buffer (make-array 200 :element-type '(unsigned-byte 8)))
+  (let ((buffer (make-array *max-decl-size* :element-type '(unsigned-byte 8)))
         (pos 0))
     (labels ((fill-buffer (n)
                (setf pos (read-sequence buffer in :start pos :end (+ pos n))))
              (buffer-to-ascii ()
                (octets-to-string buffer :end pos :external-format :ascii))
-             (return-encoding (encoding &optional read)
-               (return-from xml-encoding
-                 (values encoding (or read (buffer-to-ascii)))))
-             (return-default-encoding ()
-               (return-encoding :utf-8)))
+             (return-encoding (&optional (encoding :utf-8)
+                                         (read (buffer-to-ascii)))
+               (return-from xml-encoding (values encoding read))))
       (fill-buffer 2)
       (when (search #(#xFF #xFE) buffer :end2 pos)
         (return-encoding :utf-16 ""))
       (unless (search *pi-start* buffer :end2 pos)
-        (return-default-encoding))
+        (return-encoding))
       (fill-buffer 3)
       (unless (search *xml* buffer :start2 2 :end2 pos)
-        (return-default-encoding))
-      (loop when (= pos (fill-buffer 1)) do (return-default-encoding)
+        (return-encoding))
+      (loop when (= pos (fill-buffer 1)) do (return-encoding)
          until (search *pi-end* buffer :start2 (- pos 2) :end2 pos))
       (let ((read (buffer-to-ascii)))
-        (values (or (xml-encoding-decl read) :utf-8) read)))))
+        (return-encoding (xml-encoding-decl read) read)))))
 
 (defun make-xml-stream (octet-stream)
   (multiple-value-bind (encoding read) (xml-encoding octet-stream)
